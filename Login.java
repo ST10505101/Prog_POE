152e34103885d2c55d9df8bb42195b489bab3144
ST10505101 <ammaarah.mostert@gmail.com>
2026-08-25 22:53:24 +0200

    feat: added checkPasswordComplexity method and updated string variables

--- Login.java
+++ Login.java
@@ -2,19 +2,58 @@
   */
   public class Login{
 // Private instance variables to store user data securely
-private string registeredUserName;
-private string registeredPassword;
-private string registeredCellPhoneNumber;
-  }
+private String registeredUserName;
+private String registeredPassword;
+private String registeredCellPhoneNumber;
+  
 
-/* CheckUserName: ensures that any username contains an underscore (_) and is no more than 5 characters long.
-*@param username The username input to check @return boolean True if Valid, false otherwise
+/**
+ * CheckUserName: ensures that any username contains an underscore (_) and is no more than 5 characters long.
+ * @param username The username input to check
+ * @return boolean True if valid, false otherwise
  */
-public boolean checkUserName(string username){
-    if (username != null && username.contains("_") && username.length() <= 5){
+public boolean checkUserName(String username) {
+    if (username != null && username.contains("_") && username.length() <= 5) {
         return true;
     }
     return false;
-}
- 
+    }
 
+
+/** checkPasswordComplexity: ensures passwords meet complexity rules:
+ * At least 8 characters long, contain a capital letter, a number, and a special character).
+ * @param password The Password input to check
+ * @return boolean True if criteria are met, false otherwise
+ */
+public boolean checkPasswordComplexity(String password) {
+    if (password == null || password.length() < 8) {
+        return false;
+    }
+// Password validation checks
+boolean hascapital = false;
+boolean hasNumber = false;
+boolean hasSpecial = false;
+
+// Loop
+for(int i=0; i < password.length(); i++) {
+    char ch = password.charAt(i);
+    if (Character.isUpperCase(ch)) {
+        hascapital = true;
+        } else if (Character.isDigit(ch)){
+        hasNumber = true;
+        } else if (!Character.isLetterOrDigit(ch)) {
+        hasSpecial = true;
+        }
+    }
+    return hascapital && hasNumber && hasSpecial;
+    }
+}
+
+  
+
+
+
+
+  
+
+  
